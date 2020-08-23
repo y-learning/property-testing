2594ede1e5c2fa58735203311c2c23bbb725059b
Yahya <yahyatinani@gmail.com>
2020-08-23 12:27:35 +0100

    Refactor MathKtTest in terms of FunSpec test style

--- src/test/kotlin/me/why/math/MathKtTest.kt
+++ src/test/kotlin/me/why/math/MathKtTest.kt
@@ -1,6 +1,6 @@
 package me.why.math
 
-import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.spec.style.FunSpec
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.filter
 import io.kotest.property.arbitrary.int
@@ -8,40 +8,44 @@ import io.kotest.property.arbitrary.list
 import io.kotest.property.forAll
 
 
-class MathTest : StringSpec({
-    "maxOfTwo" {
-        forAll(Arb.int(), Arb.int()) { a: Int, b: Int ->
-            val max = maxOfTwo(a, b)
+class MyTests : FunSpec({
+    context("maxOfTwo") {
+        test("returns the greater of two values `a` and `b`.") {
+            forAll(Arb.int(), Arb.int()) { a: Int, b: Int ->
+                val max = maxOfTwo(a, b)
 
-            when {
-                a >= b -> max == a
-                else -> max == b
+                when {
+                    a >= b -> max == a
+                    else -> max == b
+                }
             }
         }
     }
 
-    "maxOfInts returns null when the list is empty." {
-        forAll(Arb.list(Arb.int()).filter { it.isEmpty() }) { ints: List<Int> ->
-            val maxOfInts = maxOfInts(ints)
+    context("maxOfInts") {
+        test("returns null when the list is empty.") {
+            forAll(Arb.list(Arb.int()).filter { it.isEmpty() }) { ints: List<Int> ->
+                val maxOfInts = maxOfInts(ints)
 
-            maxOfInts == null
+                maxOfInts == null
+            }
         }
-    }
 
-    "maxOfInts returns an element that belongs to the list." {
-        forAll(Arb.list(Arb.int()).filter { it.isNotEmpty() }) { list: List<Int> ->
-            val max = maxOfInts(list)
+        test("returns an element that belongs to the list.") {
+            forAll(Arb.list(Arb.int()).filter { it.isNotEmpty() }) { list: List<Int> ->
+                val max = maxOfInts(list)
 
-            list.contains(max)
+                list.contains(max)
+            }
         }
-    }
 
-    "maxOfInts returns the max of elements" {
-        forAll(Arb.list(Arb.int()).filter { it.isNotEmpty() }) { list: List<Int> ->
-            val max = maxOfInts(list)
+        test("returns the max of elements") {
+            forAll(Arb.list(Arb.int()).filter { it.isNotEmpty() }) { list: List<Int> ->
+                val max = maxOfInts(list)
 
-            max != null &&
-                    list.fold(false) { _, i -> max >= i }
+                max != null &&
+                        list.fold(false) { _, i -> max >= i }
+            }
         }
     }
 })
